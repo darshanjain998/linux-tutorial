f97be2c6ee026b9eb8281f379f81dd112d53a21c
darshan jain <darshanjain9988@gmail.com>
2022-08-13 18:01:16 +0530

    add example for delay and watching shell script

---
+++ delay.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DELAY=$1
+
+if [ -z $DELAY ]
+then
+ echo "You must supply a delay"
+ exit 1
+fi
+
+echo "Going to sleep for $DELAY seconds"
+sleep $DELAY
+echo "we are awake now."
+
+exit 0
