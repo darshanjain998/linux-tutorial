5cafecdf31ff7bd8d7bf6f51049655d75a36b531
darshan jain <darshanjain9988@gmail.com>
2022-08-13 13:01:53 +0530

    Initial Commit

---
+++ eg1.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#!/bin/bash
+#created by darshan jain
+
+echo "linux is best os in the world"
+echo "Hello world!!"
---
+++ eg2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#!/bin/bash
+#created by darshan jain
+
+
+FIRST_NAME=Darshan 
+LAST_NAME=jain
+GREETING_MESSAGE="Good Morning Enjoy Your Day"
+
+echo hi $FIRST_NAME $LAST_NAME $GREETING_MESSAGE.
